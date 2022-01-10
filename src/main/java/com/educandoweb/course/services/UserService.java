6e1bd3c0c5c5c66f076c53a44eef17b6e95f1292
rafael.cunha <rafael.cunha@sf303298.sefaz.pe.gov.br>
2022-01-10 15:05:39 -0300

    Implemnteção da camada de serviço e registrar o componente do Spring;
    Criando o metodo findAll e o findById

---
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -0,0 +1,27 @@
+package com.educandoweb.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.course.entities.User;
+import com.educandoweb.course.repositories.UserRepository;
+
+@Service // Registrando a class como componente do Spring
+public class UserService {
+
+	@Autowired // Injetar a dependencia de forma transparente para o usuário
+	private UserRepository repository;
+
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+
+}
