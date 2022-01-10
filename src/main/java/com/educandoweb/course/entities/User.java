42205fd66909088b46c464db7ae59cec5e38579c
rafael.cunha <rafael.cunha@sf303298.sefaz.pe.gov.br>
2022-01-10 15:15:57 -0300

    CRiação da class pedido (order) e associção entre a classe order e user

--- src/main/java/com/educandoweb/course/entities/User.java
+++ src/main/java/com/educandoweb/course/entities/User.java
@@ -1,6 +1,8 @@
 package com.educandoweb.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.Entity;
@@ -19,6 +21,8 @@ public class User implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name, email, phone, password;
+
+	private List<Order> orders = new ArrayList<>();
 
 	public User() {
 	}
@@ -72,6 +76,10 @@ public class User implements Serializable {
 		this.password = password;
 	}
 
+	public List<Order> getOrders() {
+		return orders;
+	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -92,7 +100,8 @@ public class User implements Serializable {
 	@Override
 	public String toString() {
 		return "User [id=" + id + ", name=" + name + ", email=" + email + ", phone=" + phone + ", password=" + password
-				+ "]";
+				+ ", orders=" + orders + "]";
 	}
+
 
 }
