69f14be2fd328a291927ad50f668b4ad8026eabf
rafael.cunha <rafael.cunha@sf303298.sefaz.pe.gov.br>
2022-01-11 16:19:21 -0300

    Criação da entidade Category

---
+++ src/main/java/com/educandoweb/course/entities/Product.java
@@ -0,0 +1,17 @@
+package com.educandoweb.course.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+//@Entity
+//@Table(name = "tb_product")
+public class Product implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
