dc1d7747eda78b131f589467279590e5eaa6f4b6
rafael.cunha <rafael.cunha@sf303298.sefaz.pe.gov.br>
2022-01-14 14:37:54 -0300

    Inserção de usuário

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.educandoweb.course.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.services.UserService;
@@ -19,16 +23,25 @@ public class UserResource {
 	@Autowired
 	private UserService service;
 
-	@GetMapping
+	@GetMapping // Para buscar dados em urls
 	public ResponseEntity<List<User>> findAll() {
 		List<User> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 
 	@GetMapping(value = "/{id}") // Quer dizer que a url vai aceitar um tipo id
-	public ResponseEntity<User> findById(@PathVariable Long id) { //@PathVariable serve para receber o id pela URL
+	public ResponseEntity<User> findById(@PathVariable Long id) { // @PathVariable serve para receber o id pela URL
 		User user = service.findById(id);
 		return ResponseEntity.ok().body(user);
 	}
 
+	@PostMapping // Para inserção de daods
+	public ResponseEntity<User> insert(@RequestBody User obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+
+		// Poderia ser abaixo, mas normalmente se trabalha com o cod 201
+		// return ResponseEntity.ok().body(user);
+	}
 }
--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -24,4 +24,11 @@ public class UserService {
 		return obj.get();
 	}
 
+	public User insert(User obj) {
+		return repository.save(obj);
+	}
+	
+	public void delete(Long id) {
+		repository.findById(id);
+	}
 }
