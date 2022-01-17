f66e05cebe291a3cccc66a6434570f3605aed6c2
rafael.cunha <rafael.cunha@sf303298.sefaz.pe.gov.br>
2022-01-17 10:25:07 -0300

    Tratativa de exceções no metodo delete

--- src/main/java/com/educandoweb/course/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/educandoweb/course/resources/exceptions/ResourceExceptionHandler.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.educandoweb.course.services.exceptions.DatabaseExecption;
 import com.educandoweb.course.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice // É a que vai interceptar as exceções que irão acontecer! Para possiveis
@@ -25,6 +26,15 @@ public class ResourceExceptionHandler implements Serializable {
 	public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
 		String error = "Resource not found!";
 		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(),
+				request.getRequestURI());
+		return ResponseEntity.status(status).body(err);
+	}
+
+	@ExceptionHandler(DatabaseExecption.class) // A anotação que irá interceder na exceção da classe
+	public ResponseEntity<StandardError> database(DatabaseExecption e, HttpServletRequest request) {
+		String error = "Database erro!";
+		HttpStatus status = HttpStatus.BAD_REQUEST;
 		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(),
 				request.getRequestURI());
 		return ResponseEntity.status(status).body(err);
---
+++ src/main/java/com/educandoweb/course/services/exceptions/DatabaseExecption.java
@@ -0,0 +1,14 @@
+package com.educandoweb.course.services.exceptions;
+
+public class DatabaseExecption extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public DatabaseExecption(String msg) {
+		super(msg);
+	}
+
+}
